ea4463e82c6be6fe54acf5f0078fa26172a16427
Perry <48571870+PerryW11@users.noreply.github.com>
2020-10-01 00:08:35 -0400

    Starting main menu and Kalonymus

---
+++ Everstorm/Intermediate/Build/Win64/EverstormEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Mindl/Documents/GitHub/Everstorm/Everstorm/Intermediate/Build/Win64/EverstormEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Everstorm/Intermediate/Build/Win64/UE4Editor/Inc/Everstorm/Kalonymus.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef EVERSTORM_Kalonymus_generated_h
+#error "Kalonymus.generated.h already included, missing '#pragma once' in Kalonymus.h"
+#endif
+#define EVERSTORM_Kalonymus_generated_h
+
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_SPARSE_DATA
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_RPC_WRAPPERS
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAKalonymus(); \
+	friend struct Z_Construct_UClass_AKalonymus_Statics; \
+public: \
+	DECLARE_CLASS(AKalonymus, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Everstorm"), NO_API) \
+	DECLARE_SERIALIZER(AKalonymus)
+
+
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAKalonymus(); \
+	friend struct Z_Construct_UClass_AKalonymus_Statics; \
+public: \
+	DECLARE_CLASS(AKalonymus, APawn, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/Everstorm"), NO_API) \
+	DECLARE_SERIALIZER(AKalonymus)
+
+
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AKalonymus(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AKalonymus) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AKalonymus); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AKalonymus); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AKalonymus(AKalonymus&&); \
+	NO_API AKalonymus(const AKalonymus&); \
+public:
+
+
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AKalonymus(AKalonymus&&); \
+	NO_API AKalonymus(const AKalonymus&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AKalonymus); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AKalonymus); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AKalonymus)
+
+
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_PRIVATE_PROPERTY_OFFSET
+#define Everstorm_Source_Everstorm_Kalonymus_h_9_PROLOG
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_PRIVATE_PROPERTY_OFFSET \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_SPARSE_DATA \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_RPC_WRAPPERS \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_INCLASS \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Everstorm_Source_Everstorm_Kalonymus_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_PRIVATE_PROPERTY_OFFSET \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_SPARSE_DATA \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_INCLASS_NO_PURE_DECLS \
+	Everstorm_Source_Everstorm_Kalonymus_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> EVERSTORM_API UClass* StaticClass<class AKalonymus>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Everstorm_Source_Everstorm_Kalonymus_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
